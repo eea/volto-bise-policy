bea0cf1572496ef159d5b12c59b1c20cb9948ee7
tedw87 <teodorvoicu87@gmail.com>
2023-04-28 16:29:17 +0300

    update with getFieldUrl function

--- src/components/manage/Blocks/NewsletterSignup/index.js
+++ src/components/manage/Blocks/NewsletterSignup/index.js
@@ -1,24 +1,26 @@
-import imageSVG from '@plone/volto/icons/image.svg';
-import NavigationBlockView from './View';
-import NavigationBlockEdit from './Edit';
+import imageSVG from "@plone/volto/icons/image.svg";
+import NavigationBlockView from "./View";
+import NavigationBlockEdit from "./Edit";
+import { getFieldURL } from "../../../../helpers";
 
 export default (config) => {
   config.blocks.blocksConfig.newsletterSignup = {
-    id: 'newsletterSignup',
-    title: 'Newsletter signup',
+    id: "newsletterSignup",
+    title: "Newsletter signup",
     icon: imageSVG,
-    group: 'common',
+    group: "common",
     view: NavigationBlockView,
     edit: NavigationBlockEdit,
     restricted: false,
     mostUsed: false,
     variations: [
       {
-        id: 'default',
-        title: 'Default',
+        id: "default",
+        title: "Default",
         isDefault: true,
         render: (props) => {
-          const { hasImage, imgSrc } = props.data;
+          const { hasImage } = props.data;
+          const imgSrc = getFieldURL(props.data.imgSrc);
 
           return (
             <>
@@ -31,9 +33,9 @@ export default (config) => {
         },
       },
       {
-        id: 'enisa_newsletter',
-        title: 'Enisa newsletter',
-        render: () => 'is enisa newsletter',
+        id: "enisa_newsletter",
+        title: "Enisa newsletter",
+        render: () => "is enisa newsletter",
         schemaEnhancer: ({ schema, formData, intl }) => {
           return schema;
         },
