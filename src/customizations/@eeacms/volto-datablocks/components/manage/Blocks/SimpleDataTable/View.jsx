d9c33169f69b6558adecb52d1df40040740a536a
ana-oprea <anaop3212@gmail.com>
2024-05-28 17:08:48 +0300

    shadow volto-datablocks, View.jsx from volto and fix css for header menu

---
+++ src/customizations/@eeacms/volto-datablocks/components/manage/Blocks/SimpleDataTable/View.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import { compose } from 'redux';
+import config from '@plone/volto/registry';
+import { DefaultView } from '@eeacms/volto-datablocks/components/manage/Blocks/SimpleDataTable/templates/default';
+import { VisibilitySensor } from '@eeacms/volto-datablocks/components';
+import { connectToProviderData } from '@eeacms/volto-datablocks/hocs';
+import { serializeNodes } from '@eeacms/volto-datablocks/serialize';
+
+import './styles.less';
+
+const getAlignmentOfColumn = (col, idx) => {
+  return typeof col !== 'string' && col.textAlign
+    ? col.textAlign
+    : idx === 0
+    ? 'left'
+    : 'right';
+};
+
+const getNameOfColumn = (col) => {
+  return typeof col === 'string' ? col : col.column;
+};
+
+const getTitleOfColumn = (col) => {
+  return typeof col === 'string' ? col : col.title || getNameOfColumn(col);
+};
+
+const selectedColumnValidator = (allColDefs) => (colDef) => {
+  return typeof colDef === 'string'
+    ? false
+    : allColDefs.includes(colDef?.column);
+};
+
+const getProviderDataLength = (provider_data) => {
+  return provider_data
+    ? provider_data[Object.keys(provider_data)[0]]?.length || 0
+    : 0;
+};
+
+const SimpleDataTableView = (props) => {
+  const { data = {}, pagination = {} } = props;
+  const {
+    columns,
+    description,
+    has_pagination = false,
+    max_count = 5,
+    placeholder = 'No results',
+    show_header = false,
+    template = 'default',
+  } = data;
+
+  const provider_data =
+    (pagination.data[pagination.activePage]
+      ? pagination.data[pagination.activePage]
+      : pagination.activePage !== pagination.prevPage
+      ? pagination.data[pagination.prevPage]
+      : null) || props.provider_data;
+  const provider_data_length = getProviderDataLength(provider_data);
+
+  const TableView =
+    config.blocks.blocksConfig.simpleDataConnectedTable?.templates?.[template]
+      ?.view || DefaultView;
+
+  // TODO: sorting
+  const row_size = has_pagination
+    ? !Object.keys(pagination.data).includes(pagination.activePage)
+      ? provider_data_length
+      : Math.min(pagination.itemsPerPage, provider_data_length) || 0
+    : max_count > 0
+    ? Math.min(max_count, provider_data_length)
+    : provider_data_length;
+
+  const providerColumns = Object.keys(provider_data || {});
+  const showAllColumns = !Array.isArray(columns) || columns.length === 0;
+  const validator = selectedColumnValidator(providerColumns);
+  const selectedColumns = showAllColumns
+    ? providerColumns
+    : columns.filter(validator);
+
+  const tableData = provider_data;
+
+  return (
+    <div className="simple-data-table">
+      <div className={`table-title ${data.underline ? 'title-border' : ''}`}>
+        {description ? serializeNodes(description) : ''}
+      </div>
+      <TableView
+        {...props}
+        has_pagination={has_pagination}
+        placeholder={placeholder}
+        provider_data={provider_data}
+        provider_data_length={provider_data_length}
+        row_size={row_size}
+        selectedColumns={selectedColumns}
+        show_header={show_header}
+        tableData={tableData}
+        getAlignmentOfColumn={getAlignmentOfColumn}
+        getNameOfColumn={getNameOfColumn}
+        getTitleOfColumn={getTitleOfColumn}
+      />
+    </div>
+  );
+};
+
+export { SimpleDataTableView };
+
+const SimpleDataTableViewWrapper = compose(
+  connectToProviderData((props) => {
+    const { max_count = 5 } = props.data;
+    return {
+      provider_url: props.data?.provider_url,
+      pagination: {
+        enabled: props.data.has_pagination,
+        itemsPerPage:
+          typeof max_count !== 'number' ? parseInt(max_count) : max_count,
+      },
+    };
+  }),
+)(SimpleDataTableView);
+
+export default (props) => {
+  return (
+    <VisibilitySensor offset={{ top: -150, bottom: -150 }}>
+      <SimpleDataTableViewWrapper {...props} />
+    </VisibilitySensor>
+  );
+};
---
+++ src/customizations/@eeacms/volto-datablocks/components/manage/Blocks/SimpleDataTable/index.js
@@ -0,0 +1,64 @@
+import tableSVG from '@plone/volto/icons/table.svg';
+import SimpleDataTableView from './View';
+import SimpleDataTableEdit from './Edit';
+import {
+  DefaultView,
+  defaultSchema,
+} from '@eeacms/volto-datablocks/components/manage/Blocks/SimpleDataTable/templates/default';
+import { SmartView, smartSchema } from './templates/smart';
+import { ColoredTableView, coloredTableSchema } from './templates/colored';
+
+export default (config) => {
+  config.blocks.blocksConfig.simpleDataConnectedTable = {
+    id: 'simpleDataConnectedTable',
+    title: 'Data Table',
+    icon: tableSVG,
+    group: 'data_blocks',
+    view: SimpleDataTableView,
+    edit: SimpleDataTableEdit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+    security: {
+      addPermission: [],
+      view: [],
+    },
+    templates: {
+      default: {
+        title: 'Default',
+        view: DefaultView,
+        schema: defaultSchema,
+      },
+      smart: {
+        title: 'Smart',
+        view: SmartView,
+        schema: smartSchema,
+      },
+      colored_table: {
+        title: 'Colored table',
+        view: ColoredTableView,
+        schema: coloredTableSchema,
+      },
+      ...(config.blocks.blocksConfig.simpleDataConnectedTable?.templates || {}),
+    },
+    available_colors: [
+      '#FCE0E0',
+      '#E2F1E4',
+      '#EB2631',
+      '#F06630',
+      '#FBDC35',
+      '#41B34F',
+      '#8EC54A',
+      '#D5DF3F',
+      '#EFEFEF',
+      '#EB9694',
+      '#FEF3BD',
+      '#C1E1C5',
+      '#BEDADC',
+      '#C4DEF6',
+      '#BED3F3',
+      '#D4C4FB',
+    ],
+  };
+  return config;
+};
