8f5a5f57fbe1630839658850ec4f593e84836739
nileshgulia1 <nileshgulia@gmail.com>
2026-02-04 18:51:03 +0530

    fix NRR widgets and Case studies Views refs #296409

--- src/components/Widgets/NRRWidgets.jsx
+++ src/components/Widgets/NRRWidgets.jsx
@@ -1,3 +1,5 @@
+import { UniversalLink } from '@plone/volto/components';
+import { Button, Popup } from 'semantic-ui-react';
 export const NRRTypologyOfMeasuresView = ({ value }) => {
   let parsedValue = value;
 
@@ -21,11 +23,15 @@ export const NRRTypologyOfMeasuresView = ({ value }) => {
         <b>Related typology of measures: </b>
       </span>
       <ul>
-        {items.map((item, index) => (
-          <li key={item['@id'] ?? index}>
-            <p>{item.title}</p>
-          </li>
-        ))}
+        {items.map((item) => {
+          const description = item.title.split(' - ')?.pop();
+          const code = item.title.split(' - ').slice(0, 2).join(' - ');
+          return (
+            <li key={item['@id']}>
+              <Popup content={description} trigger={<b>{code}</b>} />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
@@ -59,6 +65,69 @@ export const NRRArticleView = ({ value }) => {
           {items.indexOf(item) < items.length - 1 ? ', ' : ''}
         </span>
       ))}
+    </div>
+  );
+};
+
+export const NRRrelatedCaseStudiesView = ({ value }) => {
+  let parsedValue = value;
+
+  if (typeof value === 'string') {
+    try {
+      parsedValue = JSON.parse(value);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  const items = Array.isArray(parsedValue)
+    ? parsedValue
+    : parsedValue?.value || [];
+
+  if (!items || items.length === 0) return null;
+
+  return (
+    <div className="eunis-widget-viewZ">
+      <span>
+        <b>Related Case Studies: </b>
+      </span>
+      <ul>
+        {items.map((item, index) => (
+          <li key={item['@id'] ?? index}>
+            <UniversalLink href={item['@id']}>{item.title}</UniversalLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export const NRRMeasuresImplementedView = ({ value }) => {
+  let parsedValue = value;
+
+  if (typeof value === 'string') {
+    try {
+      parsedValue = JSON.parse(value);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  const items = Array.isArray(parsedValue)
+    ? parsedValue
+    : parsedValue?.value || [];
+
+  if (!items || items.length === 0) return null;
+
+  return (
+    <div className="eunis-widget-viewZ">
+      <ul>
+        {items.map((item, index) => (
+          <li key={item['@id'] ?? index}>
+            <UniversalLink href={item['@id']}>{item.title}</UniversalLink>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
