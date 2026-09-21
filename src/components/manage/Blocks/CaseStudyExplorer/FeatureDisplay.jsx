19e4a0fa357391ab96a12c676ae79c3501f0a2ba
laszlocseh <csehlaszlo91@gmail.com>
2026-09-21 12:33:03 +0300

    refactor(CaseStudyExplorer): use Image component in FeatureDisplay

--- src/components/manage/Blocks/CaseStudyExplorer/FeatureDisplay.jsx
+++ src/components/manage/Blocks/CaseStudyExplorer/FeatureDisplay.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Image from '@plone/volto/components/theme/Image/Image';
 
 export default function FeatureDisplay({ feature }) {
   return feature ? (
     <div id="csepopup">
       {feature.image ? (
-        <img
+        <Image
           className="csepopup-image"
           src={feature.image}
           alt={feature.title || ''}
